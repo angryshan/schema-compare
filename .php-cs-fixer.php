49f1dd76245e3db586a260bf72a53680d10abcf5
angryshan <angryshan@qq.com>
2026-06-30 18:21:17 +0800

    新增 PHP CS Fixer 配置文件和 PHPUnit 配置文件，以支持代码格式化和测试环境的设置。

---
+++ .php-cs-fixer.php
@@ -0,0 +1,89 @@
+<?php
+
+$header = '';
+
+return (new \PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR2' => true,
+        '@Symfony' => true,
+        '@DoctrineAnnotation' => true,
+        '@PhpCsFixer' => true,
+        'header_comment' => [
+            'comment_type' => 'PHPDoc',
+            'header' => $header,
+            'separate' => 'none',
+            'location' => 'after_declare_strict',
+        ],
+        'array_syntax' => [
+            'syntax' => 'short',
+        ],
+        'list_syntax' => [
+            'syntax' => 'short',
+        ],
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'blank_line_before_statement' => [
+            'statements' => [
+                'declare',
+            ],
+        ],
+        'ordered_imports' => [
+            'imports_order' => [
+                'class', 'function', 'const',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
+        'single_line_comment_style' => [
+            'comment_types' => [
+            ],
+        ],
+        'yoda_style' => [
+            'always_move_variable' => false,
+            'equal' => false,
+            'identical' => false,
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+        'multiline_whitespace_before_semicolons' => [
+            'strategy' => 'no_multi_line',
+        ],
+        'constant_case' => [
+            'case' => 'lower',
+        ],
+        'class_attributes_separation' => true,
+        'combine_consecutive_unsets' => true,
+        'declare_strict_types' => true,
+        'linebreak_after_opening_tag' => true,
+        'lowercase_static_reference' => true,
+        'no_useless_else' => true,
+        'no_unused_imports' => false,
+        'not_operator_with_space' => false,
+        'ordered_class_elements' => true,
+        'php_unit_strict' => false,
+        'phpdoc_separation' => false,
+        'single_quote' => true,
+        'standardize_not_equals' => true,
+        'multiline_comment_opening_closing' => true,
+        'no_superfluous_phpdoc_tags' => false,
+        'phpdoc_summary' => false,
+        'phpdoc_no_package' => false,
+        'general_phpdoc_annotation_remove' => false,
+        'not_operator_with_successor_space' => false,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_functions' => false,
+            'import_constants' => false,
+        ],
+        'fully_qualified_strict_types' => false,
+    ])
+    ->setFinder(
+        \PhpCsFixer\Finder::create()
+            ->exclude('test')
+            ->exclude('vendor')
+            ->in(__DIR__ . '/src')
+    )
+    ->setLineEnding("\r\n")
+    ->setUsingCache(false);
